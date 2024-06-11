3df5623ef9332f21379e097b5b437e17051ae7f3
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-11 17:43:47 +0200

    ! all

--- RGBStripFirmware.ino
+++ RGBStripFirmware.ino
@@ -1,20 +1,15 @@
 #include <SoftwareSerial.h>
 
-//#define DEBUG
 #define RED_PIN 5
 #define GREEN_PIN 6
 #define BLUE_PIN 9
 
-// SoftwareSerial bluetooth(3, 2);
-
 void setup() {
-  // bluetooth.begin(115200);
   Serial.begin(2000000);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
 
-  // strip test
   setColor(0x88, 0x88, 0x88);
   delay(50);
   setColor(0, 0, 0);
